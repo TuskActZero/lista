87258922ac8541d9f55f43c48cb2e155e1715495
carlos <pikmam@gmail.com>
2021-01-03 21:46:32 -0300

    asd

--- lista/urls.py
+++ lista/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include("listado.urls")),
     path('admin/', admin.site.urls),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+]
